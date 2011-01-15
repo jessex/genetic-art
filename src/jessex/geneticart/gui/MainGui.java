67145364bd0fbe30ca98620ebe994a308d138e58
Joshua Essex <essexjosh@gmail.com>
2011-01-15 16:41:46 -0500

    Updates

--- src/jessex/geneticart/gui/MainGui.java
+++ src/jessex/geneticart/gui/MainGui.java
@@ -17,7 +17,7 @@ public class MainGui extends JFrame implements ActionListener {
     DisplayGui ac;
 
     JPanel settings, control;
-    JButton reset, quit, load;
+    JButton reset, quit, load, export, imports;
     DoubleJSlider picAdd, picDel, picMove, polyAdd, polyDel;
     DoubleJSlider vertMax, vertMid, vertMin;
     DoubleJSlider colorRate, alphaRate;
@@ -27,7 +27,7 @@ public class MainGui extends JFrame implements ActionListener {
     ButtonGroup set;
 
     boolean isRunning;
-    private final Dimension FRAMEDIMENSION = new Dimension(450,560);
+    private final Dimension FRAMEDIMENSION = new Dimension(450,310);
     private final Dimension BUTTONDIMENSION = 
             new Dimension ((FRAMEDIMENSION.width-30)/3,40);
     private final Dimension SLIDERDIMENSION =
@@ -46,9 +46,11 @@ public class MainGui extends JFrame implements ActionListener {
         createGui();
     }
 
+    @SuppressWarnings("static-access")
     public void createGui() {
         setSize(FRAMEDIMENSION);
         setMinimumSize(FRAMEDIMENSION);
+        setLocation(200,200);
         setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new GridBagLayout());
@@ -58,7 +60,7 @@ public class MainGui extends JFrame implements ActionListener {
         settings.setBorder(new TitledBorder("Mutation Rates and Settings"));
         settings.setLayout(new GridBagLayout());
         settings.setPreferredSize(new Dimension(FRAMEDIMENSION.width,
-                FRAMEDIMENSION.height-150));
+                FRAMEDIMENSION.height-50));
         GridBagConstraints p = new GridBagConstraints();
 
         /* ********************************************************************
@@ -72,6 +74,7 @@ public class MainGui extends JFrame implements ActionListener {
         pa.setFont(LABELFONT);
         pa.setHorizontalAlignment(SwingConstants.LEFT);
         p.gridx = p.gridy = 0;
+        p.anchor = p.WEST;
         p.insets = new Insets(5,5,0,10);
         settings.add(pa, p);
 
@@ -90,6 +93,7 @@ public class MainGui extends JFrame implements ActionListener {
         settings.add(pm, p);
 
         picAdd = new DoubleJSlider(0,100,0,1000);
+        picAdd.setToolTipText("Rate of polygon addition");
         picAdd.setPreferredSize(SLIDERDIMENSION);
         picAdd.addChangeListener(new ChangeListener(){
             @Override
@@ -104,6 +108,7 @@ public class MainGui extends JFrame implements ActionListener {
         settings.add(picAdd, p);
 
         picDel = new DoubleJSlider(0,100,0,1000);
+        picDel.setToolTipText("Rate of polygon deletion");
         picDel.setPreferredSize(SLIDERDIMENSION);
         picDel.addChangeListener(new ChangeListener(){
             @Override
@@ -117,6 +122,7 @@ public class MainGui extends JFrame implements ActionListener {
         settings.add(picDel, p);
 
         picMove = new DoubleJSlider(0,100,0,1000);
+        picMove.setToolTipText("Rate of polygon movement between layers");
         picMove.setPreferredSize(SLIDERDIMENSION);
         picMove.addChangeListener(new ChangeListener(){
             @Override
@@ -154,6 +160,7 @@ public class MainGui extends JFrame implements ActionListener {
         settings.add(vMn, p);
 
         vertMax = new DoubleJSlider(0,100,0,1000);
+        vertMax.setToolTipText("Rate of maximal vertex movement");
         vertMax.setPreferredSize(SLIDERDIMENSION);
         vertMax.addChangeListener(new ChangeListener(){
             @Override
@@ -168,6 +175,7 @@ public class MainGui extends JFrame implements ActionListener {
         settings.add(vertMax, p);
 
         vertMid = new DoubleJSlider(0,100,0,1000);
+        vertMid.setToolTipText("Rate of medium vertex movement");
         vertMid.setPreferredSize(SLIDERDIMENSION);
         vertMid.addChangeListener(new ChangeListener(){
             @Override
@@ -181,6 +189,7 @@ public class MainGui extends JFrame implements ActionListener {
         settings.add(vertMid, p);
 
         vertMin = new DoubleJSlider(0,100,0,1000);
+        vertMin.setToolTipText("Rate of minimal vertex movement");
         vertMin.setPreferredSize(SLIDERDIMENSION);
         vertMin.addChangeListener(new ChangeListener(){
             @Override
@@ -212,6 +221,7 @@ public class MainGui extends JFrame implements ActionListener {
         settings.add(poD, p);
 
         polyAdd = new DoubleJSlider(0,100,0,1000);
+        polyAdd.setToolTipText("Rate of vertex addition");
         polyAdd.setPreferredSize(SLIDERDIMENSION);
         polyAdd.addChangeListener(new ChangeListener(){
             @Override
@@ -226,6 +236,7 @@ public class MainGui extends JFrame implements ActionListener {
         settings.add(polyAdd, p);
 
         polyDel = new DoubleJSlider(0,100,0,1000);
+        polyDel.setToolTipText("Rate of vertex deletion");
         polyDel.setPreferredSize(SLIDERDIMENSION);
         polyDel.addChangeListener(new ChangeListener(){
             @Override
@@ -239,7 +250,7 @@ public class MainGui extends JFrame implements ActionListener {
         settings.add(polyDel, p);
 
         // *************************** FOURTH ROW ******************************
-
+        /*
         cR = new JLabel("Color Rate:");
         cR.setFont(LABELFONT);
         cR.setHorizontalAlignment(SwingConstants.LEFT);
@@ -256,6 +267,7 @@ public class MainGui extends JFrame implements ActionListener {
         settings.add(cI, p);
 
         colorRate = new DoubleJSlider(0,100,0,1000);
+        colorRate.setToolTipText("Rate of color mutation");
         colorRate.setPreferredSize(SLIDERDIMENSION);
         colorRate.addChangeListener(new ChangeListener(){
             @Override
@@ -270,6 +282,7 @@ public class MainGui extends JFrame implements ActionListener {
         settings.add(colorRate, p);
 
         colorInt = new JSlider(JSlider.HORIZONTAL, 10, 255, Settings.colorIntensity);
+        colorInt.setToolTipText("Intensity of color mutation");
         colorInt.setPreferredSize(SLIDERDIMENSION);
         colorInt.addChangeListener(new ChangeListener(){
             @Override
@@ -282,51 +295,73 @@ public class MainGui extends JFrame implements ActionListener {
         p.insets = new Insets(5,0,0,10);
         settings.add(colorInt, p);
 
-        
+        // *************************** FIFTH ROW *******************************
 
-        
+        aR = new JLabel("Alpha Rate:");
+        aR.setFont(LABELFONT);
+        aR.setHorizontalAlignment(SwingConstants.LEFT);
+        p.gridx = 0;
+        p.gridy = 8;
+        p.insets = new Insets(5,5,0,10);
+        settings.add(aR, p);
 
+        aI = new JLabel("Alpha Intensity:");
+        aI.setFont(LABELFONT);
+        aI.setHorizontalAlignment(SwingConstants.LEFT);
+        p.gridx = 1;
+        p.insets = new Insets(5,0,0,10);
+        settings.add(aI, p);
 
+        alphaRate = new DoubleJSlider(0,100,0,1000);
+        alphaRate.setToolTipText("Rate of transparency mutation");
+        alphaRate.setPreferredSize(SLIDERDIMENSION);
+        alphaRate.addChangeListener(new ChangeListener(){
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                aR.setText("Alpha Rate: " + DF.format(alphaRate.getScaledValue()));
+            }
+        });
+        aR.setText("Alpha Rate: " + DF.format(alphaRate.getScaledValue()));
+        p.gridx = 0;
+        p.gridy = 9;
+        p.insets = new Insets(5,5,0,10);
+        settings.add(alphaRate, p);
 
-        /* ********************************************************************
-         * ***************** END OF SETTINGS PANEL COMPONENTS *****************
-         * ********************************************************************
-         */
-
-        g.gridx = g.gridy = 0;
-        g.gridwidth = 3;
-        g.insets = new Insets(5,5,10,5); //Insets - top left bottom right
-        add(settings, g);
-
-        control = new JPanel(); //SETTINGS PANEL
-        control.setBorder(new TitledBorder("Control"));
-        control.setLayout(new GridBagLayout());
-        control.setPreferredSize(new Dimension(FRAMEDIMENSION.width,
-                150));
-        GridBagConstraints c = new GridBagConstraints();
-
-        /* ********************************************************************
-         * ***************** START OF CONTROL PANEL COMPONENTS ****************
-         * ********************************************************************
-         */
+        alphaInt = new JSlider(JSlider.HORIZONTAL, Settings.minColorTransparency,
+                Settings.maxColorTransparency, 50);
+        alphaInt.setToolTipText("Intensity of transparency mutation");
+        alphaInt.setPreferredSize(SLIDERDIMENSION);
+        alphaInt.addChangeListener(new ChangeListener(){
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                aI.setText("Alpha Intensity: " + alphaInt.getValue());
+            }
+        });
+        aI.setText("Alpha Intensity: " + alphaInt.getValue());
+        p.gridx = 1;
+        p.insets = new Insets(5,0,0,10);
+        settings.add(alphaInt, p);
+        */
+        // *************************** SIXTH ROW *******************************
 
         iP = new JLabel("Min Polygons:");
         iP.setFont(LABELFONT);
         iP.setHorizontalAlignment(SwingConstants.LEFT);
-        c.gridx = 0;
-        c.gridy = 0;
-        c.insets = new Insets(5,10,0,10);
-        control.add(iP, c);
+        p.gridx = 0;
+        p.gridy = 6;
+        p.insets = new Insets(5,10,0,10);
+        settings.add(iP, p);
 
         mP = new JLabel("Max Polygons:");
         mP.setFont(LABELFONT);
         mP.setHorizontalAlignment(SwingConstants.LEFT);
-        c.gridx = 1;
-        c.insets = new Insets(5,0,0,10);
-        control.add(mP, c);
+        p.gridx = 1;
+        p.insets = new Insets(5,0,0,10);
+        settings.add(mP, p);
 
         minPoly = new JSlider(JSlider.HORIZONTAL, Settings.MINPOLMIN,
                 Settings.MINPOLMAX, Settings.minPolygons);
+        minPoly.setToolTipText("Minimum amount of polygons in the picture");
         minPoly.setPreferredSize(SLIDERDIMENSION);
         minPoly.addChangeListener(new ChangeListener(){
             @Override
@@ -335,13 +370,14 @@ public class MainGui extends JFrame implements ActionListener {
             }
         });
         iP.setText("Min Polygons: " + minPoly.getValue());
-        c.gridx = 0;
-        c.gridy = 1;
-        c.insets = new Insets(5,10,0,10);
-        control.add(minPoly, c);
+        p.gridx = 0;
+        p.gridy = 7;
+        p.insets = new Insets(5,10,0,10);
+        settings.add(minPoly, p);
 
         maxPoly = new JSlider(JSlider.HORIZONTAL, Settings.MAXPOLMIN,
                 Settings.MAXPOLMAX, Settings.maxPolygons);
+        maxPoly.setToolTipText("Maximum amount of polygons in the picture");
         maxPoly.setPreferredSize(SLIDERDIMENSION);
         maxPoly.addChangeListener(new ChangeListener(){
             @Override
@@ -350,27 +386,30 @@ public class MainGui extends JFrame implements ActionListener {
             }
         });
         mP.setText("Max Polygons: " + maxPoly.getValue());
-        c.gridx = 1;
-        c.insets = new Insets(5,0,0,10);
-        control.add(maxPoly, c);
+        p.gridx = 1;
+        p.insets = new Insets(5,0,0,10);
+        settings.add(maxPoly, p);
+
+        // *************************** SEVENTH ROW *****************************
 
         iV = new JLabel("Min Vertices:");
         iV.setFont(LABELFONT);
         iV.setHorizontalAlignment(SwingConstants.LEFT);
-        c.gridx = 0;
-        c.gridy = 2;
-        c.insets = new Insets(5,10,0,10);
-        control.add(iV, c);
+        p.gridx = 0;
+        p.gridy = 8;
+        p.insets = new Insets(5,10,0,10);
+        settings.add(iV, p);
 
         mV = new JLabel("Max Vertices:");
         mV.setFont(LABELFONT);
         mV.setHorizontalAlignment(SwingConstants.LEFT);
-        c.gridx = 1;
-        c.insets = new Insets(5,0,0,10);
-        control.add(mV, c);
+        p.gridx = 1;
+        p.insets = new Insets(5,0,0,10);
+        settings.add(mV, p);
 
         minVert = new JSlider(JSlider.HORIZONTAL, Settings.MINPIMIN,
                 Settings.MINPIMAX, Settings.minPolyPoints);
+        minVert.setToolTipText("Minimum amount of vertices per polygon");
         minVert.setPreferredSize(SLIDERDIMENSION);
         minVert.addChangeListener(new ChangeListener(){
             @Override
@@ -379,13 +418,14 @@ public class MainGui extends JFrame implements ActionListener {
             }
         });
         iV.setText("Min Vertices: " + minVert.getValue());
-        c.gridx = 0;
-        c.gridy = 3;
-        c.insets = new Insets(5,10,0,10);
-        control.add(minVert, c);
+        p.gridx = 0;
+        p.gridy = 9;
+        p.insets = new Insets(5,10,0,10);
+        settings.add(minVert, p);
 
         maxVert = new JSlider(JSlider.HORIZONTAL, Settings.MAXPIMIN,
                 Settings.MAXPIMAX, Settings.maxPolyPoints);
+        maxVert.setToolTipText("Maximum amount of vertices per polygon");
         maxVert.setPreferredSize(SLIDERDIMENSION);
         maxVert.addChangeListener(new ChangeListener(){
             @Override
@@ -394,27 +434,50 @@ public class MainGui extends JFrame implements ActionListener {
             }
         });
         mV.setText("Max Vertices: " + maxVert.getValue());
-        c.gridx = 1;
-        c.insets = new Insets(5,0,0,10);
-        control.add(maxVert, c);
+        p.gridx = 1;
+        p.insets = new Insets(5,0,0,10);
+        settings.add(maxVert, p);
+
+
+        export = new JButton("Export");
+        export.setPreferredSize(new Dimension(BUTTONDIMENSION.width-5,
+                BUTTONDIMENSION.height-5));
+        export.setFont(BUTTONFONT);
+        export.setForeground(Color.BLUE);
+        export.addActionListener(this);
+        p.gridy = 6;
+        p.gridx = 2;
+        p.gridheight = 2;
+        p.insets = new Insets(0,0,0,5);
+        settings.add(export, p);
+
+        imports = new JButton("Import");
+        imports.setPreferredSize(new Dimension(BUTTONDIMENSION.width-5,
+                BUTTONDIMENSION.height-5));
+        imports.setFont(BUTTONFONT);
+        imports.setForeground(Color.BLUE);
+        imports.addActionListener(this);
+        p.gridy = 8;
+        settings.add(imports, p);
 
 
         /* ********************************************************************
-         * ***************** END OF CONTROL PANEL COMPONENTS ******************
+         * ***************** END OF SETTINGS PANEL COMPONENTS *****************
          * ********************************************************************
          */
 
-        g.gridy = 1;
+        g.gridx = g.gridy = 0;
+        g.anchor = g.WEST;
         g.gridwidth = 3;
-        g.insets = new Insets(0,5,10,5);
-        add(control, g);
+        g.insets = new Insets(5,5,10,5); //Insets - top left bottom right
+        add(settings, g);
 
         reset = new JButton("Reset"); //RESET BUTTON
         reset.setPreferredSize(BUTTONDIMENSION);
         reset.setFont(BUTTONFONT);
         reset.setForeground(Color.BLUE);
         reset.addActionListener(this);
-        g.gridy = 2;
+        g.gridy = 1;
         g.gridwidth = 1;
         g.insets = new Insets(0,5,5,10);
         add(reset, g);
@@ -442,8 +505,26 @@ public class MainGui extends JFrame implements ActionListener {
     }
 
 
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == reset) { //CLICKED RESET
+            
+        }
+        else if (e.getSource() == load) { //CLICKED LOAD
+            ac = new DisplayGui();
+        }
+        else if (e.getSource() == quit) { //CLICKED QUIT
+            System.exit(0);
+        }
+        else if (e.getSource() == export) { //CLICKED EXPORT
+
+        }
+        else if (e.getSource() == imports) { //CLICKED IMPORT
+
+        }
+    }
 
 
+    
     public static void main(String[] args) {
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
@@ -459,6 +540,8 @@ public class MainGui extends JFrame implements ActionListener {
         MainGui gui = new MainGui();
 
     }
+    
+    
 
     class DoubleJSlider extends JSlider {
         final int scale;
@@ -476,19 +559,6 @@ public class MainGui extends JFrame implements ActionListener {
             super.setValue((int) (value * this.scale));
         }
 
-    }
-
-
-    public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == reset) { //CLICKED RESET
-            
-        }
-        else if (e.getSource() == load) { //CLICKED LOAD
-
-        }
-        else if (e.getSource() == quit) { //CLICKED QUIT
-            System.exit(0);
-        }
     }
 
 }
